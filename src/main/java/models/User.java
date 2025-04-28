5d4015c701e072891fa738b324e85728a1f88af7
Luan Cortes <luancortes208@gmail.com>
2025-04-27 22:40:36 -0300

    feat(models): Criando a classe abstrata para ser herdada pelos usuários como cliente por exemplo.

---
+++ src/main/java/models/User.java
@@ -0,0 +1,43 @@
+package models;
+
+public sealed abstract class User permits Cliente {
+    private final Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public User(Long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
